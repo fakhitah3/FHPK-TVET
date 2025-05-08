1312e9c8887658188b64c14df00082ba6b98cd0d
fakhitah3 <fakhitah.ridzuan@gmail.com>
2025-05-08 16:09:26 +0800

    Update Industry_List.py

--- Industry_List.py
+++ Industry_List.py
@@ -11,6 +11,22 @@ try:
     
     # Group by 'TAHUN' and count the number of 'INDUSTRI'
     industri_counts = df_industri.groupby('TAHUN')['INDUSTRI'].count()
+    # Create the bar chart
+    plt.figure(figsize=(8, 6))
+    plt.bar(industri_counts.index, industri_counts.values)
+
+    # Set labels and title
+    plt.xlabel("Tahun")
+    plt.ylabel("Bilangan Industri")
+    plt.title("Bilangan Industri Tahunan")
+
+    # Customize x-axis ticks to show only 2022, 2023, 2024
+    plt.xticks([2022, 2023, 2024])
+    
+    # Display the plot using Streamlit
+    st.pyplot(plt)
+    
+    pelajar_counts = df_industri.groupby('TAHUN')['JUMLAH PELAJAR'].count()
 
     # Create the bar chart
     plt.figure(figsize=(8, 6))
