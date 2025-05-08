fe6523fde05aa475bd3af5b5cfbf395ae95baab1
fakhitah3 <fakhitah.ridzuan@gmail.com>
2025-05-08 15:21:52 +0800

    Update PLO_sum.py

--- PLO_sum.py
+++ PLO_sum.py
@@ -33,10 +33,10 @@ plo_sums = df_filtered[plo_columns].sum()/4
 # Use Streamlit's columns to display metrics in one line
 col1, col2, col3, col4 = st.columns(4)
 
-col1.metric(label="PLO 2", value=f"{plo_sums['PLO 2']:.2f}", delta="0", help="Total value for PLO 2", border=True)
-col2.metric(label="PLO 3", value=f"{plo_sums['PLO 3']:.2f}", delta="0", help="Total value for PLO 3", border=True)
-col3.metric(label="PLO 4", value=f"{plo_sums['PLO 4']:.2f}", delta="0", help="Total value for PLO 4", border=True)
-col4.metric(label="PLO 5", value=f"{plo_sums['PLO 5']:.2f}", delta="0", help="Total value for PLO 5", border=True)
+col1.metric(label="PLO 2", value=f"{plo_sums['PLO 2']:.2f}", help="Total value for PLO 2", border=True)
+col2.metric(label="PLO 3", value=f"{plo_sums['PLO 3']:.2f}", help="Total value for PLO 3", border=True)
+col3.metric(label="PLO 4", value=f"{plo_sums['PLO 4']:.2f}", help="Total value for PLO 4", border=True)
+col4.metric(label="PLO 5", value=f"{plo_sums['PLO 5']:.2f}", help="Total value for PLO 5", border=True)
   
 # Create the plot
 fig, ax = plt.subplots(figsize=(12, 6))
