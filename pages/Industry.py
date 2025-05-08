15ebcaa28cd2faf62be92cdc70d28ee535d43107
fakhitah3 <fakhitah.ridzuan@gmail.com>
2025-05-08 11:53:12 +0800

    Create Industry.py

---
+++ pages/Industry.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Main title for the app
+st.title("Streamlit Multi-Page App")
+
+# This main file doesn't need explicit navigation logic.
+# Streamlit will auto-generate the sidebar with pages from the 'pages' folder.
+st.write("Welcome to the Streamlit multi-page app. Use the sidebar to navigate.")
