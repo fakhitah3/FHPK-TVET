1257f7fc81c7f80ea5b869c064fc5c30d3f4a214
fakhitah3 <fakhitah.ridzuan@gmail.com>
2025-05-08 12:02:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import streamlit as st
 # Set the title of the app
 st.title("Streamlit Multi-Page App")
 
+st.set_page_config(
+    page_title="Faculty of Hospitality, Tourism and Wellness UMK",
+    page_icon="🏨",  # You can choose an appropriate emoji for the icon, like a hotel emoji.
+    menu_items={
+        'About': "# Made By Faculty of Hospitality, Tourism and Wellness UMK"
+    }
+)
+
 # Create a sidebar with navigation options
 page = st.sidebar.radio("Select Page", ["PLO Sum Visualization", "Overall PLO"])
 
