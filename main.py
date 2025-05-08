ca1b3191da9374166e6e2e3c14d20a57d8bbaf89
fakhitah3 <fakhitah.ridzuan@gmail.com>
2025-05-08 15:50:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.set_page_config(
     }
 )
 
-analysis = st.Page('PLO_sum.py', title='PLO by Course', icon=":material/school:)
+analysis = st.Page('PLO_sum.py', title='PLO by Course', icon=":material/school:")
 overall = st.Page('Overall_PLO.py', title="Overall PLO")
 industri = st.Page('Industry_List.py', title='Industry List', icon=":material/business:")
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
