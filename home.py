e8b99c664ed435ad1924818afa8940581d4542b2
fakhitah3 <fakhitah.ridzuan@gmail.com>
2025-05-10 00:09:58 +0800

    Update home.py

--- home.py
+++ home.py
@@ -8,15 +8,20 @@ st.image(banner_image, use_container_width =True)
 st.write(
     """
     **Program Apprentice 3u1i@FHPK** merupakan sebuah program pengajian mod industri yang dijalankan dengan pelaksanaan struktur kurikulum yang merangkumi tiga tahun pengajian akademik di UMK dan satu tahun latihan industri yang disusun secara sistematik dalam tempoh pengajian di industri terpilih. Program pengajian mod industri yang dijalankan adalah menggunakan kaedah pembelajaran berasaskan kerja (WBL) dengan menggunapakai garis panduan pelaksanaan mod pengajian 2u2i. 
+)
 
+banner_image = 'https://raw.githubusercontent.com/fakhitah3/FHPK-TVET/main/3u1i_2.jpeg' 
+st.image(banner_image, use_container_width =True)
+
+# Add the main paragraph and explanation
+st.write(
+    """
     Program Apprentice 3u1i@FHPK telah mula ditawarkan bermula ambilan (kohort) tahun 2018 dan telah memasuki kohort ke-4 pada tahun 2025. Sehingga kini FHPK telah berjaya menghasilkan seramai **202 graduan** daripada program pengajian Mod Industri 3u1i. 
 
     Sebanyak **14 syarikat** yang terdiri daripada tiga industri utama iaitu **perhotelan**, **pelancongan** dan **kesejahteraan** telah terlibat secara langsung dalam penempatan pelajar bagi program ini.
     """
 )
 
-banner_image = 'https://raw.githubusercontent.com/fakhitah3/FHPK-TVET/main/3u1i_v2.jpeg' 
-st.image(banner_image, use_container_width =True)
 
 st.markdown(
     "[Untuk maklumat lanjut, sila layari Laman Sesawang Fakulti Hospitaliti, Pelancongan dan Kesejahteraan UMK.](https://fhpk.umk.edu.my/program-details.cfm?ref=801&name=---)"
