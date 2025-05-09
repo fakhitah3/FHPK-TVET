d29acb6d8a61851a61195a202d12b10d18965255
fakhitah3 <fakhitah.ridzuan@gmail.com>
2025-05-09 23:13:42 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,26 @@
 import streamlit as st
 
-# Set the title of the app
-st.title("Streamlit Multi-Page App")
+# Set the title and icon for the page
+st.set_page_config(
+    page_title="Program Apprentice 3u1i@FHPK",
+    page_icon="🎓", 
+)
+
+# Add a banner image at the top
+banner_image = 'your_banner_image_path.jpg'  # Replace with your banner image path
+st.image(banner_image, use_column_width=True)
+
+# Add the main paragraph and explanation
+st.write(
+    """
+    **Program Apprentice 3u1i@FHPK** merupakan sebuah program pengajian mod industri yang dijalankan dengan pelaksanaan struktur kurikulum yang merangkumi tiga tahun pengajian akademik di UMK dan satu tahun latihan industri yang disusun secara sistematik dalam tempoh pengajian di industri terpilih. Program pengajian mod industri yang dijalankan adalah menggunakan kaedah pembelajaran berasaskan kerja (WBL) dengan menggunapakai garis panduan pelaksanaan mod pengajian 2u2i. 
+
+    Program Apprentice 3u1i@FHPK telah mula ditawarkan bermula ambilan (kohort) tahun 2018 dan telah memasuki kohort ke-4 pada tahun 2025. Sehingga kini FHPK telah berjaya menghasilkan seramai **202 graduan** daripada program pengajian Mod Industri 3u1i. 
+
+    Sebanyak **14 syarikat** yang terdiri daripada tiga industri utama iaitu **perhotelan**, **pelancongan** dan **kesejahteraan** telah terlibat secara langsung dalam penempatan pelajar bagi program ini.
+    """
+)
+
+st.markdown(
+    "[For more details, click here to visit the program page](https://fhpk.umk.edu.my/program-details.cfm?ref=801&name=---)"
+)
