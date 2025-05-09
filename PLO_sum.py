9ea0b24b66736eef8271c78fe85e788a23b0ac33
fakhitah3 <fakhitah.ridzuan@gmail.com>
2025-05-09 23:40:31 +0800

    Update PLO_sum.py

--- PLO_sum.py
+++ PLO_sum.py
@@ -17,218 +17,213 @@ st.write(
     - **Kesejahteraan**
     """
 )
+with st.expander("Jumlah PLO"):
+    
+with st.expander("Pelancongan"):   
+    try:
+      df = pd.read_csv('https://raw.githubusercontent.com/fakhitah3/FHPK-TVET/refs/heads/main/Data/PLO%20Analysis%20SAP.csv')
+      print(df.head()) # Print the first few rows to verify
+    except FileNotFoundError:
+      print("Error: File not found. Please upload the file to your current working directory or provide the correct path.")
+    except Exception as e:
+      print(f"An error occurred: {e}")
+    
+    # Create a dropdown menu for year selection
+    year_selectionSAP = st.selectbox("Select Year", ["2023", "2024"])
+    
+    # Filter data based on the selected year
+    df_filtered = df[df['Tahun'] == int(year_selectionSAP)]
+    
+    # Grouped bar chart
+    plo_columns = ['PLO 2', 'PLO 3', 'PLO 4', 'PLO 5']
+    subjects = df_filtered['Subjek'].unique()
+    
+    width = 0.15
+    x = range(len(subjects))
+    
+    # Now, calculate the total sum for each PLO and display them as metrics
+    plo_sums = df_filtered[plo_columns].sum()/4
+    
+    # Use Streamlit's columns to display metrics in one line
+    col1, col2, col3, col4 = st.columns(4)
+    
+    col1.metric(label="PLO 2", value=f"{plo_sums['PLO 2']:.2f}", help="PLO 2: Cognitive Skill", border=True)
+    col2.metric(label="PLO 3", value=f"{plo_sums['PLO 3']:.2f}", help="PLO 3: Digital Skill", border=True)
+    col3.metric(label="PLO 4", value=f"{plo_sums['PLO 4']:.2f}", help="PLO 4: Interpersonal Skill", border=True)
+    col4.metric(label="PLO 5", value=f"{plo_sums['PLO 5']:.2f}", help="PLO 5: Communication Skill", border=True)
+      
+    # Create the plot
+    fig = go.Figure()
+    
+    # Add each PLO as a separate trace (bar group)
+    for i, plo in enumerate(plo_columns):
+        values = df_filtered.groupby('Subjek')[plo].sum()
+        fig.add_trace(go.Bar(
+            x=[val + i * width for val in x],
+            y=values,
+            name=plo,
+            # text=values,  # Display the value on hover
+            # textposition='outside',  # Position the text outside the bar
+            hoverinfo='y',  # Show text on hover
+        ))
+    
+    # Update layout to set axis labels and chart title
+    fig.update_layout(
+        title=f"Prestasi PLO bagi Tahun {year_selectionSAP}",
+        xaxis_title="Subjek",
+        yaxis_title="Nilai",
+        xaxis=dict(
+            tickmode='array',
+            tickvals=[val + width for val in x],
+            ticktext=subjects,
+        ),
+        barmode='group',  # Display the bars in a grouped manner
+        legend=dict(x=1.05, y=1),  # Move legend outside the chart
+        margin=dict(r=100),  # Add margin for the legend
+    )
+    
+    # Display the Plotly chart using Streamlit
+    st.plotly_chart(fig)
 
-# Add a header to the Streamlit app
-st.title("Pelancongan")  # This creates a title at the top of the page
+with st.expander("Hospitality"):   
+    try:
+      dx = pd.read_csv('https://raw.githubusercontent.com/fakhitah3/FHPK-TVET/refs/heads/main/Data/PLO%20Analysis%20SAH.csv')
+      print(dx.head()) # Print the first few rows to verify
+    except FileNotFoundError:
+      print("Error: File not found. Please upload the file to your current working directory or provide the correct path.")
+    except Exception as e:
+      print(f"An error occurred: {e}")
+    
+    
+    # Create a dropdown menu for year selection
+    year_selectionSAH = st.selectbox("Select Year", ["2022", "2023", "2024"])
+    
+    # Filter data based on the selected year
+    dx_filtered = dx[dx['Tahun'] == int(year_selectionSAH)]
+    
+    # Grouped bar chart
+    plo_columns = ['PLO 2', 'PLO 3', 'PLO 4', 'PLO 5']
+    subjects = dx_filtered['Subjek'].unique()
+    
+    width = 0.15
+    x = range(len(subjects))
+    
+    # Now, calculate the total sum for each PLO and display them as metrics
+    plo_sums = dx_filtered[plo_columns].sum()/4
+    
+    # Use Streamlit's columns to display metrics in one line
+    col1, col2, col3, col4 = st.columns(4)
+    
+    col1.metric(label="PLO 2", value=f"{plo_sums['PLO 2']:.2f}", help="PLO 2: Cognitive Skill", border=True)
+    col2.metric(label="PLO 3", value=f"{plo_sums['PLO 3']:.2f}", help="PLO 3: Digital Skill", border=True)
+    col3.metric(label="PLO 4", value=f"{plo_sums['PLO 4']:.2f}", help="PLO 4: Interpersonal Skill", border=True)
+    col4.metric(label="PLO 5", value=f"{plo_sums['PLO 5']:.2f}", help="PLO 5: Communication Skill", border=True)
+    
+    
+    width = 0.15
+    x = range(len(subjects))
+    
+    # Create the plot
+    fig = go.Figure()
+    
+    # Add each PLO as a separate trace (bar group)
+    for i, plo in enumerate(plo_columns):
+        values = df_filtered.groupby('Subjek')[plo].sum()
+        fig.add_trace(go.Bar(
+            x=[val + i * width for val in x],
+            y=values,
+            name=plo,
+            # text=values,  # Display the value on hover
+            # textposition='outside',  # Position the text outside the bar
+            hoverinfo='y',  # Show text on hover
+        ))
+    
+    # Update layout to set axis labels and chart title
+    fig.update_layout(
+        title=f"Prestasi PLO bagi Tahun {year_selectionSAH}",
+        xaxis_title="Subjek",
+        yaxis_title="Nilai",
+        xaxis=dict(
+            tickmode='array',
+            tickvals=[val + width for val in x],
+            ticktext=subjects,
+        ),
+        barmode='group',  # Display the bars in a grouped manner
+        legend=dict(x=1.05, y=1),  # Move legend outside the chart
+        margin=dict(r=100),  # Add margin for the legend
+    )
+    
+    # Display the Plotly chart using Streamlit
+    st.plotly_chart(fig)
 
-try:
-  df = pd.read_csv('https://raw.githubusercontent.com/fakhitah3/FHPK-TVET/refs/heads/main/Data/PLO%20Analysis%20SAP.csv')
-  print(df.head()) # Print the first few rows to verify
-except FileNotFoundError:
-  print("Error: File not found. Please upload the file to your current working directory or provide the correct path.")
-except Exception as e:
-  print(f"An error occurred: {e}")
-
-# Create a dropdown menu for year selection
-year_selectionSAP = st.selectbox("Select Year", ["2023", "2024"])
-
-# Filter data based on the selected year
-df_filtered = df[df['Tahun'] == int(year_selectionSAP)]
-
-# Grouped bar chart
-plo_columns = ['PLO 2', 'PLO 3', 'PLO 4', 'PLO 5']
-subjects = df_filtered['Subjek'].unique()
-
-width = 0.15
-x = range(len(subjects))
-
-# Now, calculate the total sum for each PLO and display them as metrics
-plo_sums = df_filtered[plo_columns].sum()/4
-
-# Use Streamlit's columns to display metrics in one line
-col1, col2, col3, col4 = st.columns(4)
-
-col1.metric(label="PLO 2", value=f"{plo_sums['PLO 2']:.2f}", help="PLO 2: Cognitive Skill", border=True)
-col2.metric(label="PLO 3", value=f"{plo_sums['PLO 3']:.2f}", help="PLO 3: Digital Skill", border=True)
-col3.metric(label="PLO 4", value=f"{plo_sums['PLO 4']:.2f}", help="PLO 4: Interpersonal Skill", border=True)
-col4.metric(label="PLO 5", value=f"{plo_sums['PLO 5']:.2f}", help="PLO 5: Communication Skill", border=True)
-  
-# Create the plot
-fig = go.Figure()
-
-# Add each PLO as a separate trace (bar group)
-for i, plo in enumerate(plo_columns):
-    values = df_filtered.groupby('Subjek')[plo].sum()
-    fig.add_trace(go.Bar(
-        x=[val + i * width for val in x],
-        y=values,
-        name=plo,
-        # text=values,  # Display the value on hover
-        # textposition='outside',  # Position the text outside the bar
-        hoverinfo='y',  # Show text on hover
-    ))
-
-# Update layout to set axis labels and chart title
-fig.update_layout(
-    title=f"Prestasi PLO bagi Tahun {year_selectionSAP}",
-    xaxis_title="Subjek",
-    yaxis_title="Nilai",
-    xaxis=dict(
-        tickmode='array',
-        tickvals=[val + width for val in x],
-        ticktext=subjects,
-    ),
-    barmode='group',  # Display the bars in a grouped manner
-    legend=dict(x=1.05, y=1),  # Move legend outside the chart
-    margin=dict(r=100),  # Add margin for the legend
-)
-
-# Display the Plotly chart using Streamlit
-st.plotly_chart(fig)
-
-# Add a header to the Streamlit app
-st.title("Hospitaliti")  # This creates a title at the top of the page
-
-try:
-  dx = pd.read_csv('https://raw.githubusercontent.com/fakhitah3/FHPK-TVET/refs/heads/main/Data/PLO%20Analysis%20SAH.csv')
-  print(dx.head()) # Print the first few rows to verify
-except FileNotFoundError:
-  print("Error: File not found. Please upload the file to your current working directory or provide the correct path.")
-except Exception as e:
-  print(f"An error occurred: {e}")
-
-
-# Create a dropdown menu for year selection
-year_selectionSAH = st.selectbox("Select Year", ["2022", "2023", "2024"])
-
-# Filter data based on the selected year
-dx_filtered = dx[dx['Tahun'] == int(year_selectionSAH)]
-
-# Grouped bar chart
-plo_columns = ['PLO 2', 'PLO 3', 'PLO 4', 'PLO 5']
-subjects = dx_filtered['Subjek'].unique()
-
-width = 0.15
-x = range(len(subjects))
-
-# Now, calculate the total sum for each PLO and display them as metrics
-plo_sums = dx_filtered[plo_columns].sum()/4
-
-# Use Streamlit's columns to display metrics in one line
-col1, col2, col3, col4 = st.columns(4)
-
-col1.metric(label="PLO 2", value=f"{plo_sums['PLO 2']:.2f}", help="PLO 2: Cognitive Skill", border=True)
-col2.metric(label="PLO 3", value=f"{plo_sums['PLO 3']:.2f}", help="PLO 3: Digital Skill", border=True)
-col3.metric(label="PLO 4", value=f"{plo_sums['PLO 4']:.2f}", help="PLO 4: Interpersonal Skill", border=True)
-col4.metric(label="PLO 5", value=f"{plo_sums['PLO 5']:.2f}", help="PLO 5: Communication Skill", border=True)
-
-
-width = 0.15
-x = range(len(subjects))
-
-# Create the plot
-fig = go.Figure()
-
-# Add each PLO as a separate trace (bar group)
-for i, plo in enumerate(plo_columns):
-    values = df_filtered.groupby('Subjek')[plo].sum()
-    fig.add_trace(go.Bar(
-        x=[val + i * width for val in x],
-        y=values,
-        name=plo,
-        # text=values,  # Display the value on hover
-        # textposition='outside',  # Position the text outside the bar
-        hoverinfo='y',  # Show text on hover
-    ))
-
-# Update layout to set axis labels and chart title
-fig.update_layout(
-    title=f"Prestasi PLO bagi Tahun {year_selectionSAH}",
-    xaxis_title="Subjek",
-    yaxis_title="Nilai",
-    xaxis=dict(
-        tickmode='array',
-        tickvals=[val + width for val in x],
-        ticktext=subjects,
-    ),
-    barmode='group',  # Display the bars in a grouped manner
-    legend=dict(x=1.05, y=1),  # Move legend outside the chart
-    margin=dict(r=100),  # Add margin for the legend
-)
-
-# Display the Plotly chart using Streamlit
-st.plotly_chart(fig)
-
-# Add a header to the Streamlit app
-st.title("Kesejahteraan")  # This creates a title at the top of the page
-
-try:
-  dy = pd.read_csv('https://raw.githubusercontent.com/fakhitah3/FHPK-TVET/refs/heads/main/Data/PLO%20Analysis%20SAS.csv')
-  print(dy.head()) # Print the first few rows to verify
-except FileNotFoundError:
-  print("Error: File not found. Please upload the file to your current working directory or provide the correct path.")
-except Exception as e:
-  print(f"An error occurred: {e}")
-
-
-# Create a dropdown menu for year selection
-year_selectionSAS = st.selectbox("Select Year", ["2023", "2024"], key="year_dropdown")
-
-# Filter data based on the selected year
-dy_filtered = dy[dy['Tahun'] == int(year_selectionSAS)]
-
-# Grouped bar chart
-plo_columns = ['PLO 2', 'PLO 3', 'PLO 4', 'PLO 5']
-subjects = dy_filtered['Subjek'].unique()
-
-
-width = 0.15
-x = range(len(subjects))
-
-# Now, calculate the total sum for each PLO and display them as metrics
-plo_sums = dy_filtered[plo_columns].sum()/4
-
-# Use Streamlit's columns to display metrics in one line
-col1, col2, col3, col4 = st.columns(4)
-
-col1.metric(label="PLO 2", value=f"{plo_sums['PLO 2']:.2f}", help="PLO 2: Cognitive Skill", border=True)
-col2.metric(label="PLO 3", value=f"{plo_sums['PLO 3']:.2f}", help="PLO 3: Digital Skill", border=True)
-col3.metric(label="PLO 4", value=f"{plo_sums['PLO 4']:.2f}", help="PLO 4: Interpersonal Skill", border=True)
-col4.metric(label="PLO 5", value=f"{plo_sums['PLO 5']:.2f}", help="PLO 5: Communication Skill", border=True)
-  
-  
-width = 0.15
-x = range(len(subjects))
-
-# Create the plot
-fig = go.Figure()
-
-# Add each PLO as a separate trace (bar group)
-for i, plo in enumerate(plo_columns):
-    values = df_filtered.groupby('Subjek')[plo].sum()
-    fig.add_trace(go.Bar(
-        x=[val + i * width for val in x],
-        y=values,
-        name=plo,
-        # text=values,  # Display the value on hover
-        # textposition='outside',  # Position the text outside the bar
-        hoverinfo='y',  # Show text on hover
-    ))
-
-# Update layout to set axis labels and chart title
-fig.update_layout(
-    title=f"Prestasi PLO bagi Tahun {year_selectionSAS}",
-    xaxis_title="Subjek",
-    yaxis_title="Nilai",
-    xaxis=dict(
-        tickmode='array',
-        tickvals=[val + width for val in x],
-        ticktext=subjects,
-    ),
-    barmode='group',  # Display the bars in a grouped manner
-    legend=dict(x=1.05, y=1),  # Move legend outside the chart
-    margin=dict(r=100),  # Add margin for the legend
-)
-
-# Display the Plotly chart using Streamlit
-st.plotly_chart(fig)
+with st.expander("Kesejahteraan"):   
+    try:
+      dy = pd.read_csv('https://raw.githubusercontent.com/fakhitah3/FHPK-TVET/refs/heads/main/Data/PLO%20Analysis%20SAS.csv')
+      print(dy.head()) # Print the first few rows to verify
+    except FileNotFoundError:
+      print("Error: File not found. Please upload the file to your current working directory or provide the correct path.")
+    except Exception as e:
+      print(f"An error occurred: {e}")
+    
+    
+    # Create a dropdown menu for year selection
+    year_selectionSAS = st.selectbox("Select Year", ["2023", "2024"], key="year_dropdown")
+    
+    # Filter data based on the selected year
+    dy_filtered = dy[dy['Tahun'] == int(year_selectionSAS)]
+    
+    # Grouped bar chart
+    plo_columns = ['PLO 2', 'PLO 3', 'PLO 4', 'PLO 5']
+    subjects = dy_filtered['Subjek'].unique()
+    
+    
+    width = 0.15
+    x = range(len(subjects))
+    
+    # Now, calculate the total sum for each PLO and display them as metrics
+    plo_sums = dy_filtered[plo_columns].sum()/4
+    
+    # Use Streamlit's columns to display metrics in one line
+    col1, col2, col3, col4 = st.columns(4)
+    
+    col1.metric(label="PLO 2", value=f"{plo_sums['PLO 2']:.2f}", help="PLO 2: Cognitive Skill", border=True)
+    col2.metric(label="PLO 3", value=f"{plo_sums['PLO 3']:.2f}", help="PLO 3: Digital Skill", border=True)
+    col3.metric(label="PLO 4", value=f"{plo_sums['PLO 4']:.2f}", help="PLO 4: Interpersonal Skill", border=True)
+    col4.metric(label="PLO 5", value=f"{plo_sums['PLO 5']:.2f}", help="PLO 5: Communication Skill", border=True)
+      
+      
+    width = 0.15
+    x = range(len(subjects))
+    
+    # Create the plot
+    fig = go.Figure()
+    
+    # Add each PLO as a separate trace (bar group)
+    for i, plo in enumerate(plo_columns):
+        values = df_filtered.groupby('Subjek')[plo].sum()
+        fig.add_trace(go.Bar(
+            x=[val + i * width for val in x],
+            y=values,
+            name=plo,
+            # text=values,  # Display the value on hover
+            # textposition='outside',  # Position the text outside the bar
+            hoverinfo='y',  # Show text on hover
+        ))
+    
+    # Update layout to set axis labels and chart title
+    fig.update_layout(
+        title=f"Prestasi PLO bagi Tahun {year_selectionSAS}",
+        xaxis_title="Subjek",
+        yaxis_title="Nilai",
+        xaxis=dict(
+            tickmode='array',
+            tickvals=[val + width for val in x],
+            ticktext=subjects,
+        ),
+        barmode='group',  # Display the bars in a grouped manner
+        legend=dict(x=1.05, y=1),  # Move legend outside the chart
+        margin=dict(r=100),  # Add margin for the legend
+    )
+    
+    # Display the Plotly chart using Streamlit
+    st.plotly_chart(fig)
