984318eb1f420e9c252daf587cd9b27aa41160a8
fakhitah3 <fakhitah.ridzuan@gmail.com>
2025-05-10 00:03:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ st.set_page_config(
 )
 
 analysis = st.Page('PLO_sum.py', title='PLO by Course', icon=":material/school:")
-overall = st.Page('Overall_PLO.py', title="Overall PLO")
+#overall = st.Page('Overall_PLO.py', title="Overall PLO")
 industri = st.Page('Industry_List.py', title='Industry List', icon=":material/business:")
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
         {
-            "Menu": [home, analysis, overall, industri]
+            "Menu": [home, analysis, industri]
         }
     )
 
